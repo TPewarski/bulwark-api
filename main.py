a07f4580275f410e570b1001c94dbc4b126b7f27
TPewarski <tjp2122@gmail.com>
2023-12-25 22:30:42 -0500

    Return a mock portfolio

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import json
 
 
 app = FastAPI()
@@ -6,7 +7,22 @@ app = FastAPI()
 
 @app.get("/portfolio")
 async def root(tags: list[str] = []):
-    return {"message": "Hello World"}
+    #get mock data
+    f_portfolio = open("./fixtures/portfolio.json")
+    f_postions = open("./fixtures/positions.json")
+    mock_portfolio = json.load(f_portfolio)
+    mock_positions = json.load(f_postions)
+    f_portfolio.close()
+    f_postions.close()
+
+    portfolio_id = "123" # get from db and user_id from auth
+    positions = []
+    for position in mock_positions:
+        if position["portfolio_id"] == portfolio_id:
+            positions.append(position)
+    print(positions)
+
+    return {**mock_portfolio, "positions": positions}
 
 @app.get("/trade/{ticker}")
 def trade(ticker: str):
